dafd723334cf486ed9c64927c16de4f735b2b98d
John Daigneault <johndaigneault@gmail.com>
2015-12-05 15:35:53 -0500

    added options, organized code

--- slider.js
+++ slider.js
@@ -1,69 +1,79 @@
 (function($) {
 
 	$.fn.slideShow = function() {
-	    return this.each(function() {
-	
-			var $container = jQuery(this),
+	    return this.each(function( options ) {
+		    
+		    var settings = $.extend({
+		            autoScroll: true,
+		            timer: 7500,
+		            resize: false,
+		            activeClass: 'active',
+		        }, options ),
+		        
+				$container = $(this),
 			    $slider = $container.find('ul'),
-			   	$slides = $slider.children('li'),
-			   	length = $slides.size(),
+			    $slides = $slider.children('li'),
+			    length = $slides.size(),
 						
-			   	index = 0,
+			    index = 0,
 			   	
-			   	// Fading animation
-			   	slideTo = function(idx) {
+			    slideTo = function(idx) {
 					$slides
-						.removeClass('active')
+						.removeClass( settings.activeClass )
 						.eq(idx)
 						.queue(function(next){
-							jQuery(this).addClass('active');
+							$(this).addClass( settings.activeClass );
 							next();
 						});
 					index = idx;
-			    },
-			    
-			    // Height Resizing
-			    resizeContainer = function(idx) {
-				    $slider.height( $slides.eq(idx).height() );
 			    };
 			
-			var timer = setInterval( switchSlides, 7500);
+			if ( settings.resize ) {
+				
+			    var resizeContainer = function(idx) {
+				    $slider.height( $slides.eq(idx).height() );
+			    };
+			    
+			}
 			
-			function switchSlides() {
-				var idx = $slides.filter('.active').index(),
-			    	prevIdx = idx - 1 < 0 ? length - 1 : index - 1;
-					nextIdx = idx + 1 < length ? index + 1 : 0;
-				slideTo(nextIdx);
+			if ( settings.autoScroll ) {
+			    
+				var timer = setInterval( nextSlide, settings.timer );
+				
+				function nextSlide() {
+					var idx = $slides.filter('.active').index(),
+						nextIdx = idx + 1 < length ? index + 1 : 0;
+					slideTo(nextIdx);
+				}
+				
 			}
 			
 			$slider.on("swipeleft", function() {
 				var idx = $slides.filter('.active').index(),
 					nextIdx = idx + 1 < length ? index + 1 : 0;
 				slideTo(nextIdx);
-				clearInterval(timer);
+				if ( settings.autoScroll ) {
+					clearInterval(timer);
+				}
 			});
 			
 			$slider.on("swiperight", function() {
 				var idx = $slides.filter('.active').index(),
 					prevIdx = idx - 1 < 0 ? length - 1 : index - 1;
 				slideTo(prevIdx);
-				clearInterval(timer);
+				if ( settings.autoScroll ) {
+					clearInterval(timer);
+				}
 			});
 			
 			$slider.imagesLoaded(function() {
-				slideTo(0);
+				slideTo(index);
+				if ( settings.resize ) {
+					resizeContainer(index);
+				}
 			});
 			
-			if($container.hasClass('resize')) {
-				resizeContainer(0);
-				
-				$slider.imagesLoaded(function() {
-					resizeContainer(index);
-				});
-			}
-			
 		});
-		
 	};
 		
 })(jQuery);
